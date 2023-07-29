962accd07271820f173502591df4aba257158724
SimBed <dansimbed@gmail.com>
2023-07-29 19:00:00 +0100

    write own headroom javascript

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import "./navbar-headroom.js"
 import * as bootstrap from "bootstrap"
 import "chartkick/chart.js"
